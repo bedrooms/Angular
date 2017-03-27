9560db39ed08d73a5d94cd06b71c9ddf59ae3c9a
RGonzalezs <evilholic@hotmail.com>
2017-03-27 09:47:43 -0500

    cambios

--- Site/app/scripts/controllers/main.js
+++ Site/app/scripts/controllers/main.js
@@ -8,8 +8,24 @@
  */
 var app = angular.module('testAngularSiteApp', '');
 
-app.controller('MainCtrl',function ($scope, $http)
+app.controller('MainCtrl',function ()
 {    
+    var vm = this;
+    vm.myInterval = 3000;
+    vm.slides = [
+        {
+        image: 'https://img.grouponcdn.com/deal/5EXVDNMDEe1mtyEK6Pgp/ZC-1057x634/v1/c700x420.jpg'
+        },
+        {
+        image: 'https://img.grouponcdn.com/deal/5EXVDNMDEe1mtyEK6Pgp/ZC-1057x634/v1/c700x420.jpg'
+        },
+        {
+        image: 'https://img.grouponcdn.com/deal/5EXVDNMDEe1mtyEK6Pgp/ZC-1057x634/v1/c700x420.jpg'
+        },
+        {
+        image: 'https://img.grouponcdn.com/deal/5EXVDNMDEe1mtyEK6Pgp/ZC-1057x634/v1/c700x420.jpg'
+        }
+    ];
    
 });
   
